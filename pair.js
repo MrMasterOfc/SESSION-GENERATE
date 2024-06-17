635b27f8110add290ee7ad909f8a827d3a2a0e5a
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-06-16 21:51:22 -0700

    👨‍💻_Mr Sahan Ofc_👨‍💻

--- pair.js
+++ pair.js
@@ -54,7 +54,7 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: "" + b64data });
+               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: "MASTER-MD" + b64data });
 
                let SIGMA_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
