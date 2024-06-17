2474c2bba383b2ea0e0abb315537a1c1542fd99e
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-06-16 21:51:57 -0700

    👨‍💻_Mr Sahan Ofc_👨‍💻

--- qr.js
+++ qr.js
@@ -56,7 +56,7 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: "" + b64data });
+				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: "MASTER-MD" + b64data });
 	
 				   let SIGMA_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
